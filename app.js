0e24e742718f25d31b3fc359f33605ae3b1c054e
sourav <souravdeybca053@gmail.com>
2024-04-08 02:03:14 +0530

    userController error fixed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 config({
   path: "./config/config.env",
 });
@@ -17,6 +19,14 @@ app.use(
 );
 
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 // Importing and using Routes
 
 import course from "./routes/courseRoutes.js";
@@ -30,5 +40,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Site is working. CLick <a href=${process.env.FRONTEND_URL}>here</a>to visit frontend</h1>`
+  );
+});
 
 app.use(ErrorMiddleware);
